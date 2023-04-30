b0ebca0993296699f3ac934a3efdc69021e209a5
Yarodash <yaroslav.lukyanets@gmail.com>
2023-04-30 19:50:42 +0300

    Added themes, transitions, hover effects

--- src/app.js
+++ src/app.js
@@ -1,15 +1,36 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './app.scss';
 import Header from './header';
 import CalculatorFsm from './calculatorFsm';
 import Display from './display';
 import Keyboard from './keyboard';
 
+function useTransition(state, time) {
+  const [last, setLast] = useState(state);
+  const [transition, setTransition] = useState(false);
+
+  useEffect(() => {
+    setTransition(true);
+
+    const timeout = setTimeout(() => {
+      setLast(state);
+      setTransition(false);
+    }, time);
+
+    return () => {
+      clearTimeout(timeout);
+    }
+  }, [state]);
+
+  return transition || last !== state;
+}
+
 function App() {
   const states = ["1", "2", "3"];
 
   const [fsm, setFsm] = useState(new CalculatorFsm());
   const [theme, setTheme] = useState(states[0]);
+  const themeTransition = useTransition(theme, 350);
 
   const onClick = (event) => {
     let newFsm = fsm.clone();
@@ -18,7 +39,7 @@ function App() {
   };
 
   return (
-    <div className={`app theme-${theme}`}>
+    <div className={`app theme-${theme}` + (themeTransition ? " theme-changing" : "")}>
       <div className="container">
         <Header
           states={states}
